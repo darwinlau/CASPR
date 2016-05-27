6c16b49c58a315d6ed6e990be1dc026b7305f5b3
Darwin Lau <darwinlau@hotmail.com>
2016-05-27 14:47:39 +0800

    + Commented out the error code printing to avoid spamming, need to be replaced with warnings in the future maybe + Moved the warning to the InverseDynamicsSimulator

--- src/Analysis/InverseDynamics/Solvers/QP/id_qp_matlab.m
+++ src/Analysis/InverseDynamics/Solvers/QP/id_qp_matlab.m
@@ -5,14 +5,14 @@ function [ x_opt, exit_type ] = id_qp_matlab(A, b, A_ineq, b_ineq, A_eq, b_eq, x
         case 1
             exit_type = IDSolverExitType.NO_ERROR;
         case 0
-            fprintf('Max iteration limit reached\n');
+            %fprintf('Max iteration limit reached\n');
             exit_type = IDSolverExitType.ITERATION_LIMIT_REACHED;
         case -2
-            fprintf('Problem infeasible\n');
+            %fprintf('Problem infeasible\n');
             exit_type = IDSolverExitType.INFEASIBLE;
             x_opt = xmin;
         otherwise
-            fprintf('Other error : Code %d\n', exitflag);
+            %fprintf('Other error : Code %d\n', exitflag);
             exit_type = IDSolverExitType.SOLVER_SPECIFIC_ERROR;
     end
 end
