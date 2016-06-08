61e0283c7dfc3dd2a208f6188536a10a76b34259
Darwin Lau <darwinlau@hotmail.com>
2016-06-08 18:09:07 +0800

    - Updated file delimiters in ModelConfig finding files to work on both linux/unix and windows (MAC not tested) without the extra if case

--- data/config/models/ModelConfig.m
+++ data/config/models/ModelConfig.m
@@ -35,89 +35,89 @@ classdef ModelConfig
             switch type
                 % Planar SCDM manipulators
                 case ModelConfigType.M_2DOF_VSD
-                    cdpr_folder = '\SCDM\planar_manipulators\2_Dof_VSD\';
+                    cdpr_folder = '/SCDM/planar_manipulators/2_Dof_VSD/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, '2_Dof_VSD_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, '2_Dof_VSD_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, '2_Dof_VSD_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, '2_Dof_VSD_op.xml'];
                 case ModelConfigType.M_KNTU_PLANAR
-                    cdpr_folder = '\SCDM\planar_manipulators\KNTU_planar\';
+                    cdpr_folder = '/SCDM/planar_manipulators/KNTU_planar/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'KNTU_planar_xy_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'KNTU_planar_xy_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'KNTU_planar_xy_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'KNTU_planar_xy_op.xml'];
                 case ModelConfigType.M_SIMPLE_PLANAR_XY
-                    cdpr_folder = '\SCDM\planar_manipulators\simple_planar_xy\';
+                    cdpr_folder = '/SCDM/planar_manipulators/simple_planar_xy/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_planar_xy_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_planar_xy_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'simple_planar_xy_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'simple_planar_xy_op.xml'];
                 % Spatial SCDM manipulators
                 case ModelConfigType.M_ACROBOT
-                    cdpr_folder = '\SCDM\spatial_manipulators\ACROBOT_IRTJV\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/ACROBOT_IRTJV/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'ACROBOT_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'ACROBOT_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'ACROBOT_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'ACROBOT_op.xml'];
 				case ModelConfigType.M_COGIRO
-                    cdpr_folder = '\SCDM\spatial_manipulators\CoGiRo\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/CoGiRo/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'CoGiRo_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'CoGiRo_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'CoGiRo_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'CoGiRo_op.xml'];
                 case ModelConfigType.M_IPANEMA_2
-                    cdpr_folder = '\SCDM\spatial_manipulators\IPAnema2_FraunhoferIPA\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/IPAnema2_FraunhoferIPA/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'IPAnema2_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'IPAnema2_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'IPAnema2_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'IPAnema2_op.xml'];
                 case ModelConfigType.M_NIST_ROBOCRANE
-                    cdpr_folder = '\SCDM\spatial_manipulators\NIST_ROBOCRANE\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/NIST_ROBOCRANE/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'NIST_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'NIST_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'NIST_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'NIST_op.xml'];
                 case ModelConfigType.M_SEGESTA
-                    cdpr_folder = '\SCDM\spatial_manipulators\SEGESTA\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/SEGESTA/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'SEGESTA_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'SEGESTA_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'SEGESTA_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'SEGESTA_op.xml'];
                 case ModelConfigType.M_SIMPLE_SPATIAL
-                    cdpr_folder = '\SCDM\spatial_manipulators\simple_spatial\';
+                    cdpr_folder = '/SCDM/spatial_manipulators/simple_spatial/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_spatial_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_spatial_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'simple_spatial_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'simple_spatial_op.xml'];
                 % Spherical SCDM manipulators
                 case ModelConfigType.M_MYOROB_SHOULDER
-                    cdpr_folder = '\SCDM\spherical_manipulators\myorob_shoulder_TUM\';
+                    cdpr_folder = '/SCDM/spherical_manipulators/myorob_shoulder_TUM/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'myorob_shoulder_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'myorob_shoulder_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'myorob_shoulder_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'myorob_shoulder_op.xml'];
                 case ModelConfigType.M_SIMPLE_SPHERICAL
-                    cdpr_folder = '\SCDM\spherical_manipulators\simple_spherical\';
+                    cdpr_folder = '/SCDM/spherical_manipulators/simple_spherical/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_spherical_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'simple_spherical_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'simple_spherical_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, 'simple_spherical_op.xml'];
                 % Multi-link Cable Driven Manipulators (MCDM)
                 case ModelConfigType.M_2R_PLANAR_XZ
-                    cdpr_folder = '\MCDM\2R_planar_xz\';
+                    cdpr_folder = '/MCDM/2R_planar_xz/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, '2R_planar_xz_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, '2R_planar_xz_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, '2R_planar_xz_trajectories.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, '2R_planar_xz_op.xml'];
                 case ModelConfigType.M_NECK_8S
-                    cdpr_folder = '\MCDM\8S_neck\';
+                    cdpr_folder = '/MCDM/8S_neck/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, '8S_neck_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, '8S_neck_cables.xml'];
                     %c.trajectoriesFilename = [c.root_folder, cdpr_folder, '8S_neck_trajectories.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, '8S_neck_trajectories_eulerXYZ.xml'];
                     c.opFilename = [c.root_folder, cdpr_folder, '8S_neck_op.xml'];
                 case ModelConfigType.M_CAREX
-                    cdpr_folder = '\MCDM\CAREX\';
+                    cdpr_folder = '/MCDM/CAREX/';
                     c.bodyPropertiesFilename = [c.root_folder, cdpr_folder, 'CAREX_bodies.xml'];
                     c.cablesPropertiesFilename = [c.root_folder, cdpr_folder, 'CAREX_cables.xml'];
                     c.trajectoriesFilename = [c.root_folder, cdpr_folder, 'CAREX_trajectories.xml'];
@@ -125,15 +125,7 @@ classdef ModelConfig
                 otherwise
                     error('ModelConfig type is not defined');
             end
-            
-            % DARWIN check that we can just use the unix style.
-            if(isunix)                
-                c.bodyPropertiesFilename = strrep(c.bodyPropertiesFilename, '\', '/');
-                c.cablesPropertiesFilename = strrep(c.cablesPropertiesFilename, '\', '/');
-                c.trajectoriesFilename = strrep(c.trajectoriesFilename, '\', '/');
-                c.opFilename = strrep(c.opFilename, '\', '/');
-            end
-            
+                        
             assert(exist(c.bodyPropertiesFilename, 'file') == 2, 'Body properties file does not exist.');
             assert(exist(c.cablesPropertiesFilename, 'file') == 2, 'Cable properties file does not exist.');
             assert(exist(c.trajectoriesFilename, 'file') == 2, 'Trajectories file does not exist.');
