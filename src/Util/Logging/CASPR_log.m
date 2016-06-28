407a0d2e3ae302c62d110677febc3ed378b10861
edenjp <jonathan_p_eden@hotmail.com>
2016-06-28 10:25:06 +0800

    Updated logging files

--- src/Util/Logging/CASPR_log.m
+++ src/Util/Logging/CASPR_log.m
@@ -12,35 +12,27 @@ classdef CASPR_log
             % Test that log level is valid
             assert(isa(log_level,'CASPRLogLevel'),'log_level must be an enum from CASPRLogLevel');
             save('logs/log_level.mat','log_level','log_path');
+            % Open the log file fresh
+            if(~isempty(log_path))
+                % Reset the log file to be empty
+                fid = fopen(log_path,'w'); fclose(fid);
+            end
         end
         
         % Logging print
         function Print(str,log_level)
-            % Load the logging information and the fprintf location
-            log_struct = load('logs/log_level.mat');
-            % Determine the what to print to
-            if(isempty(log_struct.log_path))
-                fid = 1;
-                carrage_return = '\n';
-            else
-                fid = fopen(log_struct.log_path);
-                if(isunix)
-                    carrage_return = '\n';
-                else
-                    carrage_return = '\r\n';
-                end
-            end
+            [set_log_level,fid,carrage_return] = CASPR_log.Extract();            
             switch(log_level)
                 case CASPRLogLevel.DEBUG
-                    if(log_struct.log_level == CASPRLogLevel.DEBUG)
+                    if(set_log_level == CASPRLogLevel.DEBUG)
                         fprintf(fid,['DEBUG: ',str,carrage_return]);
                     end
                 case CASPRLogLevel.INFO
-                    if((log_struct.log_level == CASPRLogLevel.DEBUG)||(log_struct.log_level == CASPRLogLevel.INFO))
+                    if((set_log_level == CASPRLogLevel.DEBUG)||(set_log_level == CASPRLogLevel.INFO))
                         fprintf(fid,['INFO: ',str,carrage_return]);
                     end
                 case CASPRLogLevel.WARNING
-                    if(log_struct.log_level ~= CASPRLogLevel.ERROR)
+                    if(set_log_level ~= CASPRLogLevel.ERROR)
                         fprintf(fid,['WARNING: ',str,carrage_return]);
                         warning(str)
                     end
@@ -56,10 +48,31 @@ classdef CASPR_log
         end
         
         function Assert(condition,str)
-            if(condition)
+            [~,fid,carrage_return] = CASPR_log.Extract();
+            if(~condition)
                 fprintf(fid,['ERROR: ',str,carrage_return]);
                 error(str)
             end
+        end
+    end
+    
+    methods(Access = private, Static)
+        function [log_level,fid,carrage_return] = Extract()
+            % Load the logging information and the fprintf location
+            log_struct = load('logs/log_level.mat');
+            % Determine the what to print to
+            if(isempty(log_struct.log_path))
+                fid = 1;
+                carrage_return = '\n';
+            else
+                fid = fopen(log_struct.log_path,'a');
+                if(isunix||ismac)
+                    carrage_return = '\n';
+                else
+                    carrage_return = '\r\n';
+                end
+            end
+            log_level = log_struct.log_level;
         end
     end
 end
