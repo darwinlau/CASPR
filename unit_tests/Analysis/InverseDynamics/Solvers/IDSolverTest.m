fce6d2ed25961b6aed42d7d0c1816b904ddb54da
Darwin Lau <darwinlau@hotmail.com>
2016-07-24 15:55:31 +0800

    Added HCDM into the IDSolverTest and updated tests to check for HCDMs (not all tests pass, need to fix)

--- unit_tests/Analysis/InverseDynamics/Solvers/IDSolverTest.m
+++ unit_tests/Analysis/InverseDynamics/Solvers/IDSolverTest.m
@@ -7,7 +7,8 @@ classdef IDSolverTest < matlab.unittest.TestCase
     properties (ClassSetupParameter)
         model_config_type = struct('SCDM', TestModelConfigType.T_SCDM, ...
             'MCDM', TestModelConfigType.T_MCDM, ...
-            'Active_passive_cables', TestModelConfigType.T_ACTIVE_PASSIVE_CABLES);
+            'Active_passive_cables', TestModelConfigType.T_ACTIVE_PASSIVE_CABLES, ...
+            'HCDM', TestModelConfigType.T_HCDM);
     end
     
     properties
@@ -36,30 +37,30 @@ classdef IDSolverTest < matlab.unittest.TestCase
     
     methods (Test)
         function testQPSolver(testCase, qp_solver_type)
-            id_objective = IDObjectiveMinQuadCableForce(ones(1, testCase.modelObj.numCables));
+            id_objective = IDObjectiveMinQuadCableForce(ones(1, testCase.modelObj.numActuators));
             id_solver = IDSolverQuadProg(testCase.modelObj, id_objective, qp_solver_type);
-            [cable_f, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
-            testCase.assertIDSolverOutput(cable_f, Q_opt, id_exit_type);
+            [actuation_soln, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
+            testCase.assertIDSolverOutput(actuation_soln, Q_opt, id_exit_type);
         end
         
         function testLPSolver(testCase, lp_solver_type)
-            id_objective = IDObjectiveMinLinCableForce(ones(testCase.modelObj.numCables, 1));
+            id_objective = IDObjectiveMinLinCableForce(ones(testCase.modelObj.numActuators, 1));
             id_solver = IDSolverLinProg(testCase.modelObj, id_objective, lp_solver_type);
-            [cable_f, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
-            testCase.assertIDSolverOutput(cable_f, Q_opt, id_exit_type);
+            [actuation_soln, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
+            testCase.assertIDSolverOutput(actuation_soln, Q_opt, id_exit_type);
         end
         
         function testInfPSolver(testCase, infp_solver_type)
-            id_objective = IDObjectiveMinInfCableForce(ones(testCase.modelObj.numCables, 1));
+            id_objective = IDObjectiveMinInfCableForce(ones(testCase.modelObj.numActuators, 1));
             id_solver = IDSolverMinInfNorm(testCase.modelObj, id_objective, infp_solver_type);
-            [cable_f, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
-            testCase.assertIDSolverOutput(cable_f, Q_opt, id_exit_type);
+            [actuation_soln, Q_opt, id_exit_type] = id_solver.resolveFunction(testCase.modelObj);
+            testCase.assertIDSolverOutput(actuation_soln, Q_opt, id_exit_type);
         end
     end
     
     methods
-        function assertIDSolverOutput(testCase, cable_f, Q_opt, id_exit_type)
-            testCase.assertLength(cable_f, testCase.modelObj.numCablesActive, 'Cable forces output of wrong dimension');
+        function assertIDSolverOutput(testCase, actuation_soln, Q_opt, id_exit_type)
+            testCase.assertLength(actuation_soln, testCase.modelObj.numActuatorsActive, 'Cable forces output of wrong dimension');
             testCase.assertInstanceOf(Q_opt, 'double');
             testCase.assertInstanceOf(id_exit_type, 'IDSolverExitType');
         end
