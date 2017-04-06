5556b623fbafd2a7cc24097e88e07cbb3c6da26b
edenjp <jonathan_p_eden@hotmail.com>
2017-04-06 11:55:57 +1000

    Updated remove function in model wizard

--- GUI/CASPR_Model_Wizard.m
+++ GUI/CASPR_Model_Wizard.m
@@ -22,7 +22,7 @@ function varargout = CASPR_Model_Wizard(varargin)
 
     % Edit the above text to modify the response to help CASPR_Model_Wizard
 
-    % Last Modified by GUIDE v2.5 06-Apr-2017 09:45:33
+    % Last Modified by GUIDE v2.5 06-Apr-2017 11:41:30
 
     % Begin initialization code - DO NOT EDIT
     gui_Singleton = 1;
@@ -119,6 +119,7 @@ function add_button_Callback(~, ~, handles) %#ok<DEFNU>
     else
         ModelConfigManager.AddModelConfig(name,folder,bodies,cables,trajectories);
     end
+    set_robotlist(handles)
 end
 
 % --- Executes on button press in remove_button.
@@ -127,9 +128,14 @@ function remove_button_Callback(~, ~, handles) %#ok<DEFNU>
     % eventdata  reserved - to be defined in a future version of MATLAB
     % handles    structure with handles and user data (see GUIDATA)
     % Read the elements from the table
-    name = get(handles.name_box,'String');
+    name = get(handles.remove_box,'String');
     % Call the appropriate model config operations
-    ModelConfigManager.RemoveModelConfig(name);
+    if(get(handles.dev_toggle,'Value'))
+        ModelConfigManager.RemoveDevModelConfig(name);
+    else
+        ModelConfigManager.RemoveModelConfig(name);
+    end
+    set_robotlist(handles)
 end
 
 % --- Executes during object creation, after setting all properties.
@@ -178,11 +184,73 @@ function advanced_toggle_Callback(hObject, ~, handles) %#ok<DEFNU>
 end
 
 % --- Executes on button press in dev_toggle.
-function dev_toggle_Callback(~, ~, ~) %#ok<DEFNU>
+function dev_toggle_Callback(~, ~, handles) %#ok<DEFNU>
     % hObject    handle to dev_toggle (see GCBO)
     % eventdata  reserved - to be defined in a future version of MATLAB
     % handles    structure with handles and user data (see GUIDATA)
     
     % Hint: get(hObject,'Value') returns toggle state of dev_toggle
-    
+    set_robotlist(handles);
+end
+
+
+% --- Executes on selection change in robotlist_menu.
+function robotlist_menu_Callback(hObject, eventdata, handles)
+    % hObject    handle to robotlist_menu (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Hints: contents = cellstr(get(hObject,'String')) returns robotlist_menu contents as cell array
+    %        contents{get(hObject,'Value')} returns selected item from robotlist_menu
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function robotlist_menu_CreateFcn(hObject, ~, ~) %#ok<DEFNU>
+    % hObject    handle to robotlist_menu (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    empty - handles not created until after all CreateFcns called
+
+    % Hint: popupmenu controls usually have a white background on Windows.
+    %       See ISPC and COMPUTER.
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
+    robotlist_str = ModelConfigManager.GetModelConfigListNames();
+    set(hObject, 'Value', 1);
+    set(hObject, 'String', robotlist_str);
+end
+
+function set_robotlist(handles)
+    if(get(handles.dev_toggle,'Value'))
+        robotlist_str = ModelConfigManager.GetDevModelConfigListNames();
+    else
+        robotlist_str = ModelConfigManager.GetModelConfigListNames();
+    end
+    set(handles.robotlist_menu, 'Value', 1);
+    set(handles.robotlist_menu, 'String', robotlist_str);
+end
+
+
+function remove_box_Callback(hObject, eventdata, handles)
+    % hObject    handle to remove_box (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Hints: get(hObject,'String') returns contents of remove_box as text
+    %        str2double(get(hObject,'String')) returns contents of remove_box as a double
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function remove_box_CreateFcn(hObject, eventdata, handles)
+    % hObject    handle to remove_box (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    empty - handles not created until after all CreateFcns called
+
+    % Hint: edit controls usually have a white background on Windows.
+    %       See ISPC and COMPUTER.
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
 end
