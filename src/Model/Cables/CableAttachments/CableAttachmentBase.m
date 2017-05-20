472382e26b2d4dd2ca4003f9dbb64221593aba0c
Darwin Lau <darwinlau@hotmail.com>
2017-05-20 22:37:06 +0800

    Changed CableAttachment objects to only store r_GA and r_PG (since r_PA is not needed)

--- src/Model/Cables/CableAttachments/CableAttachmentBase.m
+++ src/Model/Cables/CableAttachments/CableAttachmentBase.m
@@ -7,10 +7,9 @@
 classdef (Abstract) CableAttachmentBase < handle
     properties (SetAccess = protected)
         link_num            % Link number that the attachment is attached to
+        r_PG                % Relative vector ^kr_{PG} from joint location to COG in local frame
         r_GA                % Relative attachment location ^kr_{GA} from COG to attachment location in local frame
-        r_PA                % Relative attachment location ^kr_{PA} from joint to attachment locations in local frame
-        
-        r_OA                % Absolute attachment locations ^0r_{OA} in INERTIAL frame
+        r_OA                % Absolute attachment locations ^0r_{OA} in INERTIAL frame        
         
         length_offset = 0   % Length offset that depends on the type of cable attachment (particularly for moving attachment points). Typically this is 0.
     end
@@ -18,11 +17,15 @@ classdef (Abstract) CableAttachmentBase < handle
     methods 
         function update(obj, bodyKinematics)
             if (obj.link_num == 0)
-                obj.r_OA = obj.r_PA;
+                obj.r_OA = obj.r_GA;
             else
                 obj.r_OA = bodyKinematics.bodies{obj.link_num}.R_0k * (bodyKinematics.bodies{obj.link_num}.r_OG + obj.r_GA);
             end
         end
+    end
+    
+    methods (Abstract)
+        updateAttachmentLocation(obj, r_A, attachment_ref_type);
     end
         
     methods (Abstract, Static)
