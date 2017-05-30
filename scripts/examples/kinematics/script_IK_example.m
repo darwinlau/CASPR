b8438d37a460c1f78fc1891627f5f53b5b1c92d7
Darwin Lau <darwinlau@hotmail.com>
2017-05-30 14:10:10 +0800

    Added an inverse kinematics script example for template

---
+++ scripts/examples/kinematics/script_IK_example.m
@@ -0,0 +1,31 @@
+% Script for inverse kinematics (IK) 
+%
+% Author        : Darwin LAU
+% Created       : 2017
+% Description    :
+
+% Set up the type of model
+model_config = ModelConfig('Example planar XY');
+cable_set_id = 'basic';
+trajectory_id = 'example_linear';
+
+% Clear the variables, command window, and all windows
+clear; clc; close all;
+
+% Load the SystemKinematics object from the XML
+modelObj = model_config.getModel(cable_set_id);
+
+% Setup the inverse kinematics simulator with the SystemKinematics object
+disp('Start Setup Simulation');
+sim = InverseKinematicsSimulator(modelObj);
+trajectory = model_config.getJointTrajectory(trajectory_id);
+
+% Run the kinematics on the desired trajectory
+disp('Start Running Simulation');
+sim.run(trajectory);
+
+% After running the simulator the data can be plotted
+% Refer to the simulator classes to see what can be plotted.
+disp('Start Plotting Simulation');
+sim.plotJointSpace();
+sim.plotCableLengths();
