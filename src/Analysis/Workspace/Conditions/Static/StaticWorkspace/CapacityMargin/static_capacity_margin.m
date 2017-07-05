6faa771ef4f1a2a1df8870965893dcd7208fe151
edenjp <jonathan_p_eden@hotmail.com>
2017-07-05 16:49:03 +1000

    Updated capacity margins metrics to match recent changes to metric base

--- src/Analysis/Workspace/Conditions/Static/StaticWorkspace/CapacityMargin/static_capacity_margin.m
+++ src/Analysis/Workspace/Conditions/Static/StaticWorkspace/CapacityMargin/static_capacity_margin.m
@@ -10,6 +10,6 @@
 % capacity margin
 function inWorkspace = static_capacity_margin(dynamics,options)
     m = CapacityMarginMetric(dynamics.G);
-    temp_value = m.evaluate(dynamics,[],[],[]);
+    temp_value = m.evaluate(dynamics,[]);
     inWorkspace = temp_value*(temp_value>=0);
 end
