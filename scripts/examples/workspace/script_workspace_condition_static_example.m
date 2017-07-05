f228138558391fa1efaffe38fbde0eb76aed2ba6
edenjp <jonathan_p_eden@hotmail.com>
2017-07-05 16:50:50 +1000

    Added workspace examples into the scripts

---
+++ scripts/examples/workspace/script_workspace_condition_static_example.m
@@ -0,0 +1,34 @@
+% Script file to show how to use the inverse dynamics simulator
+%
+% Author        : Jonathan EDEN
+% Created       : 2015
+% Description    :
+
+% Load configs
+clc; clear; warning off; close all;
+
+% Set up the model 
+model_config    =   ModelConfig('2 DoF VSD');
+cable_set_id    =   'basic_vsd';
+modelObj        =   model_config.getModel(cable_set_id);
+
+q_begin         =   modelObj.bodyModel.q_min; q_end = modelObj.bodyModel.q_max;
+q_step          =   (modelObj.bodyModel.q_max - modelObj.bodyModel.q_min)/10;
+% Set up the workspace simulator
+% First the grid
+uGrid           =   UniformGrid(q_begin,q_end,q_step);
+% Define the workspace condition and metrics
+w_condition     =   {WorkspaceConditionBase.CreateWorkspaceCondition(WorkspaceConditionType.STATIC,[],[])};
+opt             =   WorkspaceSimulatorOptions(false,optimset('Display','off'));
+
+% Start the simulation
+disp('Start Setup Simulation');
+wsim            =   WorkspaceSimulator(modelObj,uGrid,opt);
+
+% Run the simulation
+disp('Start Running Simulation');
+wsim.run(w_condition,[]);
+
+% Plot the simulation
+disp('Start Plotting Simulation');
+wsim.plotWorkspace2([],WorkspaceConditionType.STATIC,[1,2]);
