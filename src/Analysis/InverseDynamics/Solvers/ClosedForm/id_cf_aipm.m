fab9db91fe15c242174f57bd9cb2b55461288a2d
edenjp <jonathan_p_eden@hotmail.com>
2017-09-28 12:17:49 +1000

    Updated the closed form to have different implentations of the improved case

--- src/Analysis/InverseDynamics/Solvers/ClosedForm/id_cf_aipm.m
+++ src/Analysis/InverseDynamics/Solvers/ClosedForm/id_cf_aipm.m
@@ -11,16 +11,18 @@
 % Description   : This is an implementation of the puncture method for that
 %                 is to be applied to the solution of the closed form
 %                 method.
-function [ x_opt, exit_type] = id_cf_ipm(A_eq, b_eq, x_min, x_max)
+function [ x_opt, exit_type] = id_cf_aipm(A_eq, b_eq, x_min, x_max)
      % This is the minimum norm solution if there are no constraints
     Ap = A_eq'/(A_eq*A_eq'); x_unconstrained_opt = Ap*b_eq;
     if((sum(x_unconstrained_opt - x_min < -1e-6)>0)||(sum(x_unconstrained_opt - x_max > 1e-6)>0))
         x_m = 0.5*(x_min + x_max);  
         [n,m] = size(A_eq);
         x_shift = A_eq*x_m;  
+        N = eye(m) - Ap*A_eq;
         x_temp = x_m + Ap*(b_eq - x_shift);
         index = true(m,1); 
-        x_fixed = x_m;
+        x_fixed = x_m;  
+        A_temp = A_eq;
         while((sum(x_temp - x_min < -1e-6)>0)||(sum(x_temp - x_max > 1e-6)>0))
             % Find the most violated constraint
             [min_violation,min_i] = max(x_min - x_temp);
@@ -39,12 +41,16 @@ function [ x_opt, exit_type] = id_cf_ipm(A_eq, b_eq, x_min, x_max)
                 exit_type = IDSolverExitType.INFEASIBLE;
                 return;
             end    
-            A_eq_active = A_eq(:,index);
-            Ap_active = A_eq_active'/(A_eq_active*A_eq_active');
-            b_eq_active = b_eq_active - x_fixed(i)*A_eq(:,i);
-            x_active = x_m(index) + Ap_active*(b_eq_active - A_eq_active*x_m(index));
-            x_temp(index) = x_active;
-            x_temp(~index) = x_fixed(~index);
+            % Update the solution    
+            c = -A_temp(:,i); 
+            x_shift = x_shift - c*(x_fixed(i) - x_m(i));
+            v = Ap*c;
+            beta = 1 + v(i); 
+            w = N(i,:)';
+            G = (1/beta)*w*v'*Ap;
+            Ap = Ap + G; A_temp(:,i) = zeros(n,1);
+            N = eye(m) - Ap*A_temp;
+            x_temp = x_fixed + Ap*(b_eq - x_shift);
         end
         %------------------------------------------------------------------
         % The next two lines do not appear in the original paper. They do
