1f81abe3cba89395047d7d15485909fdfd379af2
Chen Song <c.song0611@live.com>
2017-11-10 13:11:55 +0800

    Added the extended state observer.

---
+++ src/Analysis/Observer/DisturbanceObserverESO.m
@@ -0,0 +1,87 @@
+% Disturbance observver.
+%
+% Author        : Chen SONG
+% Created       : 2017
+% Description    :
+%    Disturbance observer using ESO (extended state observer): the
+%    disturbance is assumed to be in joint space acceleration form, and the
+%    control input is assumed to be the cable forces
+
+classdef DisturbanceObserverESO < ObserverBase
+
+    properties (SetAccess = private)
+        q_hat           % the estimated state
+        q_d_hat         % the estimated state
+        d_hat           % the estimated disturbance
+        pole_vec        % pole of the observer error system
+        NUM_DOFS
+        alpha_1
+        alpha_2
+        alpha_3
+    end
+
+    methods
+        % A constructor for a computed torque controller.
+        function c = DisturbanceObserverESO(dyn_model, delta_t, pole_vec)
+            c@ObserverBase(dyn_model, delta_t);
+            c.pole_vec      =   pole_vec;
+            c.NUM_DOFS      =   dyn_model.numDofs;
+            c.q_hat         =   zeros(dyn_model.numDofs, 1);
+            c.q_d_hat       =   zeros(dyn_model.numDofs, 1);
+            c.d_hat         =   zeros(dyn_model.numDofs, 1);
+            c.alpha_1       =   3*pole_vec;
+            c.alpha_2       =   3*pole_vec.*pole_vec;
+            c.alpha_3       =   pole_vec.*pole_vec.*pole_vec;
+        end
+
+        % The implementation of the abstract executeFunction for the
+        % controller class.
+        function [disturbance] = executeFunction(obj, q, q_d, u, ~)
+            % the control u here for CDPRs is assumed be the control wrench
+            
+            if obj.first_time == true
+                % this is effectively the initialization of the observer
+                obj.first_time = false;
+                obj.q_hat = q;
+                obj.q_d_hat = q_d;
+                [q_dot_hat, q_ddot_hat, disturbance_dot] = obj.observerDynmaticFunction(q, q_d, u);
+                obj.q_hat = obj.q_hat + obj.delta_t*q_dot_hat;
+                obj.q_d_hat = obj.q_d_hat + obj.delta_t*q_ddot_hat;
+                obj.d_hat = obj.d_hat + obj.delta_t*disturbance_dot;
+                disturbance = obj.d_hat;
+            else                
+                [q_dot_hat, q_ddot_hat, disturbance_dot] = obj.observerDynmaticFunction(q, q_d, u);
+                obj.q_hat = obj.q_hat + obj.delta_t*q_dot_hat;
+                obj.q_d_hat = obj.q_d_hat + obj.delta_t*q_ddot_hat;
+                obj.d_hat = obj.d_hat + obj.delta_t*disturbance_dot;
+                disturbance = obj.d_hat;
+            end            
+        end
+        function [q_dot_hat, q_ddot_hat, disturbance_dot] = observerDynmaticFunction(obj, q, ~, u)
+            % among the inputs: only the term q and u are actually used
+            %                   the control u here for CDPRs is assumed be cable forces
+            %                   q is used to derive the estimation error
+            
+            % here update the model with the estimated variables
+            obj.dynModel.update(obj.q_hat, obj.q_d_hat, zeros(obj.NUM_DOFS,1), zeros(obj.NUM_DOFS,1));
+            
+            e = (q - obj.q_hat);
+            M = obj.dynModel.M;
+%             C = obj.dynModel.C;
+%             G = obj.dynModel.G;
+            L = obj.dynModel.L;
+            
+            % SHOULD THIS INPUT BE CABLE FORCES OR WRENCHES?
+            % assume it to be cable forces first
+            % following is the backup of old way: using wrench as the
+            % control and the disturbance is also in the form of wrench
+%             q_dot_hat           = obj.q_d_hat + diag(obj.alpha_1)*e;
+%             q_ddot_hat          = M\(-C - G + u + obj.d_hat) + diag(obj.alpha_2)*e;
+%             disturbance_dot     = diag(obj.alpha_3)*e;
+            q_dot_hat           = obj.q_d_hat + diag(obj.alpha_1)*e;
+%             q_ddot_hat          = M\(-C - G - L'*u) + obj.d_hat + diag(obj.alpha_2)*e;
+            q_ddot_hat          = obj.d_hat + diag(obj.alpha_2)*e - M\L'*u;
+            disturbance_dot     = diag(obj.alpha_3)*e;
+        end
+    end
+end
