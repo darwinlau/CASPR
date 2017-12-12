60eae46a83bc7ccce5301892e8a309ace50a3377
TristanWYL <wyl12345678900@163.com>
2017-12-12 12:27:08 +0800

    Narrow the gap between fingers to make sure the distance dectector can work more efficiently

--- src/HardwareInterface/Accessories/Gripper.m
+++ src/HardwareInterface/Accessories/Gripper.m
@@ -22,7 +22,7 @@ classdef Gripper < handle
         
         BEST_HAND_ANGLE = hex2dec('5B');% For gripping
         RELEASE_HAND_ANGLE = hex2dec('5F'); % For releasing the brick
-        LOOSE_HAND_ANGLE = hex2dec('85');% 90
+        LOOSE_HAND_ANGLE = hex2dec('80');% 90
         
         
         MAX_ARM_ANGLE  = 180;
