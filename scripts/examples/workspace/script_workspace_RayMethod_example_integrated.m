460090eeebd1db1453aa006b9c76a92b0d959e7c
edenjp <jonathan_p_eden@hotmail.com>
2018-01-30 16:30:16 +0000

    Updated the script for workspaces

--- scripts/examples/workspace/script_workspace_RayMethod_example_integrated.m
+++ scripts/examples/workspace/script_workspace_RayMethod_example_integrated.m
@@ -2,26 +2,26 @@
 clc;  close all; warning off; clear all;
 
 % Set up the model  
-% model_config    =   ModelConfig('2 DoF VSD');   %    spatial7cable   BMArm_paper   BMArm_paper
-% cable_set_id    =   'basic';
-% modelObj        =   model_config.getModel(cable_set_id);
-% nsegvar= [2500;2500];      % number of discritization on each axis. if the user desire to ignore discritization on one axis its corresponding discritiaztion number can be set to zero
+model_config    =   ModelConfig('2 DoF VSD');   %    spatial7cable   BMArm_paper   BMArm_paper
+cable_set_id    =   'basic';
+modelObj        =   model_config.getModel(cable_set_id);
+nsegvar= [25;25];      % number of discritization on each axis. if the user desire to ignore discritization on one axis its corresponding discritiaztion number can be set to zero
  
 % 'spatial7cable' 
-model_config    =   DevModelConfig('spatial7cable'); nsegvar= [3, 3, 3, 6, 6, 6]';
+% model_config    =   DevModelConfig('spatial7cable'); nsegvar= [3, 3, 3, 6, 6, 6]';
 % % '4_4_CDPR_planar'
 % model_config    =   DevModelConfig('4_4_CDPR_planar'); nsegvar = [6 6 6]';
 % % 'MickMultiIFW'
 % model_config    =   DevModelConfig('MickMultiIFW'); nsegvar = [2 6 6 6]';
 
-cable_set_id    =   'original';
-modelObj        =   model_config.getModel(cable_set_id);
+% cable_set_id    =   'original';
+% modelObj        =   model_config.getModel(cable_set_id);
 
 q_begin         =   modelObj.bodyModel.q_min; q_end = modelObj.bodyModel.q_max; 
 uGrid           =   UniformGrid(q_begin,q_end,(q_end-q_begin)./(nsegvar-1),'step_size');
 % Workspace settings and conditions
-% w_condition     =   {WorkspaceRayConditionBase.CreateWorkspaceRayCondition(WorkspaceRayConditionType.WRENCH_CLOSURE,2)};
-w_condition     =   {WorkspaceRayConditionBase.CreateWorkspaceRayCondition(WorkspaceRayConditionType.INTERFERENCE,2)};
+w_condition     =   {WorkspaceRayConditionBase.CreateWorkspaceRayCondition(WorkspaceRayConditionType.WRENCH_CLOSURE,2)};
+% w_condition     =   {WorkspaceRayConditionBase.CreateWorkspaceRayCondition(WorkspaceRayConditionType.INTERFERENCE,2)};
 opt             =   RayWorkspaceSimulatorOptions(false,false);
 % Start the simulation
 disp('Start Setup Simulation');
@@ -30,3 +30,4 @@ wsim            =   RayWorkspaceSimulator(modelObj,uGrid,opt);
 % Run the simulation
 disp('Start Running Simulation');
 wsim.run(w_condition,[])
+wsim.plotGraph();
