ab0d9ceb6c894bcfae4624d7280e4572f5e1686e
TristanWYL <wyl12345678900@163.com>
2018-06-07 16:27:07 +0800

    Adjusted the parameters according to the Control Table of the PRO_M54_60_S250.

--- src/HardwareInterface/CableRobotActuator/PoCaBot/DynamixelActuator/PRO_M54_60_S250.m
+++ src/HardwareInterface/CableRobotActuator/PoCaBot/DynamixelActuator/PRO_M54_60_S250.m
@@ -1,38 +1,35 @@
 classdef PRO_M54_60_S250 < DynamixelActuatorParaBase
     properties(Constant = true)
         % These properties comply with Dynamixel Protocol 2.0
-        % This class is designed for using a Dynamixel XH430-W210, and an USB2DYNAMIXEL.
-        % To use another Dynamixel model, such as PRO series, see their details in E-Manual(support.robotis.com) and edit below variables accordingly.
+        % This class is designed for using a Dynamixel model of PRO_M54_60_S250, and an USB2DYNAMIXEL.
         % Control table address
-        ADDR_TORQUE_ENABLE          = 64;                 % Control table address is varing with Dynamixel model
+        ADDR_TORQUE_ENABLE          = 562;                 % Control table address is varing with Dynamixel model
         LEN_TORQUE_ENABLE           = 1;
         % info. related to CURRENT
         % -648 ~ 648 * 2.69[mA]
-        ADDR_GOAL_CURRENT           = 102;
-        ADDR_PRESENT_CURRENT       = 126;
+        ADDR_GOAL_CURRENT           = 604;
+        ADDR_PRESENT_CURRENT       = 621;
         LEN_GOAL_CURRENT       = 2;
         LEN_PRESENT_CURRENT    = 2;
         
         % info. related to POSITION
-        % - 1,048,575 ~ 1,048,575 * 0.088 deg = -256[rev] ~ 256[rev]
-        % 1[rev] : 0 ~ 4,095
-        ADDR_GOAL_POSITION           = 116;
-        ADDR_PRESENT_POSITION       = 132;
+        % 251,417 ~ 360 degrees
+        ADDR_GOAL_POSITION           = 596;
+        ADDR_PRESENT_POSITION       = 611;
         LEN_GOAL_POSITION       = 4;
         LEN_PRESENT_POSITION    = 4;
         
         % The profile acceleration of the dynamixel;
         % This value could be set to 100;
-        ADDR_PROFILE_ACCELERATION = 108;
+        ADDR_PROFILE_ACCELERATION = 606;
         LEN_PROFILE_ACCELERATION = 4;
         
         % The profile velocity of the dynamixel
-        % This value could be set to 112;
-        ADDR_PROFILE_VELOCITY = 112;
+        ADDR_PROFILE_VELOCITY = 600;
         LEN_PROFILE_VELOCITY = 4;
         
         % Drive mode
-        ADDR_DRIVE_MODE = 10;
+        ADDR_DRIVE_MODE = -1;
         LEN_DRIVE_MODE = 1;
         ROTATION_DIRECTION_BIT_POSITION = 0;
         
@@ -45,40 +42,44 @@ classdef PRO_M54_60_S250 < DynamixelActuatorParaBase
         OPERATING_MODE_VELOCITY = 1;
         OPERATING_MODE_POSITION = 3;
         OPERATING_MODE_EXTENDED_POSITION = 4;
-        OPERATING_MODE_CURRENT_BASED_POSITION = 5;
-        OPERATING_MODE_PWM = 16;
+        OPERATING_MODE_CURRENT_BASED_POSITION = 4;
+        OPERATING_MODE_PWM = -1;
         
         % Hardware Error Status
         % This value indicates hardware error status. For more details, please refer to the Shutdown.
-        ADDR_HARDWARE_ERROR_STATUS = 70;
+        ADDR_HARDWARE_ERROR_STATUS = 892;
         LEN_HARDWARE_ERROR_STATUS = 1;
         
         % The PID parameters of Position Control Loop
-        % Range: 0~16383 for all three paras
         % 
-        ADDR_KpD = 80;% KPD = KPD(TBL) / 16
-        ADDR_KpI = 82;% KPI = KPI(TBL) / 65536
-        ADDR_KpP = 84;% KPP = KPP(TBL) / 128
+        ADDR_KpD = 594;% KPD = KPD(TBL) / 16
+        ADDR_KpI = -1;% KPI = KPI(TBL) / 65536
+        ADDR_KpP = -1;% KPP = KPP(TBL) / 128
         
         LEN_KpD = 2;
         LEN_KpI = 2;
         LEN_KpP = 2;
 
         DXL_MINIMUM_CURRENT_VALUE  = 0;
-        DXL_MAXIMUM_CURRENT_VALUE  = 648;
+        % A = V x 33,000 /  2,048 
+        % A : Current[mA]
+        % V : Present Current or Goal Torque
+        % Maximum current = 3.0 A
+        % Torque_goal_max = A*2048/33000 = 3000*2048/33000 = 186
+        DXL_MAXIMUM_CURRENT_VALUE  = 186;
         DXL_MOVING_STATUS_THRESHOLD = 20;           % Dynamixel moving status threshold
         
-        ENCODER_COUNT_PER_TURN = 4096;
-        MAX_CURRENT = 648;% nominal value.
-        MAX_TORQUE = 1.5;%N.m
-        KpP_SCALE_FACTOR = 128;
+        ENCODER_COUNT_PER_TURN = 251417;
+        MAX_CURRENT = 186;% nominal value.
+        MAX_TORQUE = 10.1;%N.m
+        KpP_SCALE_FACTOR = 1;
         KpI_SCALE_FACTOR = 65536;
         KpD_SCALE_FACTOR = 16;
         
         % Below paras are based on experiences.
         PROFILE_ACC = 150;
         PROFILE_VEL = 300;
-        MAX_WORK_CURRENT = 400;
+        MAX_WORK_CURRENT = 100;
         KpP = 900;
         KpI = 0;
         KpD = 0;
