3d1b1ee2c771ae8039eb555f179997058d34deee
Dominic Chan <ypchan@mae.cuhk.edu.hk>
2018-06-14 10:52:30 +0200

    Support segments defined on the same link

--- src/Model/Cables/CableModels/CableSegmentModel.m
+++ src/Model/Cables/CableModels/CableSegmentModel.m
@@ -9,23 +9,32 @@ classdef CableSegmentModel < handle
     properties (SetAccess = private)
         numLinks        = -1            % The number of links
         attachments     = {}            % CEll array of two attachment locations (index 1 is the "from", index 2 is the "to")
-        CRM             = []            % Cable Routing Matrix for this cable and segment
+        attached_links  = []            % Vector of link num that the two attachements
+        
     end
         
     properties (Dependent)
         segmentVector                   % vector \mathbf{l}_{ij} in frame {0}
         length                          % length of segment
         length_offset                   % offset length that is in addition to the point to point segment depending on the attachment type (typically 0)
+        CRM                             % Cable Routing Matrix for this cable and segment
     end
         
     methods
         function ck = CableSegmentModel(attachment_s, attachment_e, numLinks)
-            ck.numLinks = numLinks;
-            ck.CRM = zeros(1, numLinks+1);
+            ck.numLinks = numLinks;            
             ck.attachments{1} = attachment_s;
             ck.attachments{2} = attachment_e;
-            ck.CRM(attachment_s.link_num+1) = -1;
-            ck.CRM(attachment_e.link_num+1) = 1;
+            ck.attached_links(1) = attachment_s.link_num;
+            ck.attached_links(2) = attachment_e.link_num;            
+        end
+        
+        function value = get.CRM(obj)
+            value = zeros(1, obj.numLinks+1);
+            if obj.attached_links(1) ~= obj.attached_links(2)
+                value(obj.attached_links(1)+1) = -1;
+                value(obj.attached_links(2)+1) = 1;  
+            end
         end
         
         function c_k = getCRMTerm(obj, k)
