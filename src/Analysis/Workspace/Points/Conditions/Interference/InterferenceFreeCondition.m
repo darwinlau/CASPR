237df19ff773ceed5d1a110ba3ae7b18f84f4402
Darwin Lau <darwinlau@hotmail.com>
2019-05-13 15:49:10 +0800

    Added evaluated metrics to interference free and input of minimum cable distance for interference

--- src/Analysis/Workspace/Points/Conditions/Interference/InterferenceFreeCondition.m
+++ src/Analysis/Workspace/Points/Conditions/Interference/InterferenceFreeCondition.m
@@ -10,22 +10,37 @@ classdef InterferenceFreeCondition < WorkspaceConditionBase
         type = WorkspaceConditionType.INTERFERENCE;
     end
     
+    properties
+        min_cable_dist              % minimum distance for cable-cable interference free
+    end
+    
     methods
         % Constructor for Interference Free WS
-        function w = InterferenceFreeCondition(method)
+        function w = InterferenceFreeCondition(method, cable_d)
             if (isempty(method))
                 w.method = InterferenceFreeMethodType.M_MINDISTANCE_CABLE_CABLE;
             else
                 w.method = method;
             end
+            w.min_cable_dist = cable_d;
         end
         
         % Evaluate the Interference Free condition return true if
         % satisified
-        function inWorkspace = evaluateFunction(obj, dynamics)
+        function inWorkspace = evaluateFunction(obj, dynamics, evaluated_metrics)
+            % Check if there's any metrics that are already evaluated that 
+            % can help to solve for the workspace condition
+            if (~isempty(evaluated_metrics))
+                for i = 1:size(evaluated_metrics, 1)  
+                    if (evaluated_metrics{i, 1}.type == WorkspaceMetricType.MIN_CABLE_CABLE_DISTANCE)
+                        inWorkspace = (evaluated_metrics{i, 2} > obj.min_cable_dist);
+                        return;
+                    end
+                end
+            end
             switch(obj.method)
                 case InterferenceFreeMethodType.M_MINDISTANCE_CABLE_CABLE
-                    inWorkspace = interference_free_mindistance_cable_cable(dynamics);
+                    inWorkspace = interference_free_mindistance_cable_cable(dynamics, obj.min_cable_dist);
                 otherwise
                     CASPR_log.Print('Interference free method is not defined',CASPRLogLevel.ERROR);
             end
