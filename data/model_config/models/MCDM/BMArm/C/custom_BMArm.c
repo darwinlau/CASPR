f000942633262bc524fa96b422fbe57095debc93
Dominic Chan <ypchan@mae.cuhk.edu.hk>
2019-08-16 15:56:24 +0800

    Make inputs of C functions less confusing

--- data/model_config/models/MCDM/BMArm/C/custom_BMArm.c
+++ data/model_config/models/MCDM/BMArm/C/custom_BMArm.c
@@ -10,48 +10,48 @@
 #include "custom_BMArm.h"
 
 // Cable Lengths
-EXPORTED_FUNCTION void customCableLengths(double* l, struct input_struct st){
-    l[0] = st.q_dot[0];    
+EXPORTED_FUNCTION void customCableLengths(double* l, double* q, double* q_dot, double* q_ddot, double* W_e){
+    l[0] = q_dot[0];    
 }
 
 // Mass-inertia matrix (Flat)
-EXPORTED_FUNCTION void customM(double* M, struct input_struct st){
+EXPORTED_FUNCTION void customM(double* M, double* q, double* q_dot, double* q_ddot, double* W_e){
     // Sample        
-    M[0] = st.q[0];
-    M[1] = st.q[1];
-    M[2] = st.q[2];
-    M[3] = st.q[3];
-    M[4] = st.q_dot[0];
-    M[5] = st.q_dot[1];
-    M[6] = st.q_dot[2];
-    M[7] = st.q_dot[3];
+    M[0] = q[0];
+    M[1] = q[1];
+    M[2] = q[2];
+    M[3] = q[3];
+    M[4] = q_dot[0];
+    M[5] = q_dot[1];
+    M[6] = q_dot[2];
+    M[7] = q_dot[3];
 }
 
 // Centrifugal and Coriolis vector
-EXPORTED_FUNCTION void customC(double* C, struct input_struct st){
+EXPORTED_FUNCTION void customC(double* C, double* q, double* q_dot, double* q_ddot, double* W_e){
     // Sample        
-    C[0] = st.q[0]*2 + st.q_dot[0];
-    C[1] = st.q[1]*2 + st.q_dot[1];
-    C[2] = st.q[2]*2 + st.q_dot[2];
-    C[3] = st.q[3]*2 + st.q_dot[3];    
+    C[0] = q[0]*2 + q_dot[0];
+    C[1] = q[1]*2 + q_dot[1];
+    C[2] = q[2]*2 + q_dot[2];
+    C[3] = q[3]*2 + q_dot[3];    
 }
 
 // Gravitational vector
-EXPORTED_FUNCTION void customG(double* G, struct input_struct st){
+EXPORTED_FUNCTION void customG(double* G, double* q, double* q_dot, double* q_ddot, double* W_e){
     // Sample        
-    G[0] = st.q[0]*3 - st.q_dot[0];
-    G[1] = st.q[1]*3 - st.q_dot[1];
-    G[2] = st.q[2]*3 - st.q_dot[2];
-    G[3] = st.q[3]*3 - st.q_dot[3];    
+    G[0] = q[0]*3 - q_dot[0];
+    G[1] = q[1]*3 - q_dot[1];
+    G[2] = q[2]*3 - q_dot[2];
+    G[3] = q[3]*3 - q_dot[3];    
 }
 
 // Joint-Cable Jacobian matrix (Flat)
-EXPORTED_FUNCTION void customL(double* L, struct input_struct st){
+EXPORTED_FUNCTION void customL(double* L, double* q, double* q_dot, double* q_ddot, double* W_e){
     // Sample        
-    L[0] = st.q[0] * st.q_dot[0];
-    L[1] = st.q[1] * st.q_dot[1];
-    L[2] = st.q[2] * st.q_dot[2];
-    L[3] = st.q[3] * st.q_dot[3];    
+    L[0] = q[0] * q_dot[0];
+    L[1] = q[1] * q_dot[1];
+    L[2] = q[2] * q_dot[2];
+    L[3] = q[3] * q_dot[3];    
 }
 
 // Needed for mex
--- data/model_config/models/MCDM/BMArm/C/custom_BMArm.h
+++ data/model_config/models/MCDM/BMArm/C/custom_BMArm.h
@@ -6,21 +6,14 @@
 #define custom_BMArm_h
 #include "shrhelp.h"
 
-struct input_struct {
-    double* q;
-    double* q_dot;
-    double* q_ddot;
-    double* W_e;
-};
-
 /* Function declarations */
 // Kinematics
-EXPORTED_FUNCTION void customCableLengths(double* l, struct input_struct);
+EXPORTED_FUNCTION void customCableLengths(double* l, double* q, double* q_dot, double* q_ddot, double* W_e);
 
 // Dynamics
-EXPORTED_FUNCTION void customM(double* M, struct input_struct);
-EXPORTED_FUNCTION void customC(double* C, struct input_struct);
-EXPORTED_FUNCTION void customG(double* G, struct input_struct);
-EXPORTED_FUNCTION void customL(double* L, struct input_struct);
+EXPORTED_FUNCTION void customM(double* M, double* q, double* q_dot, double* q_ddot, double* W_e);
+EXPORTED_FUNCTION void customC(double* C, double* q, double* q_dot, double* q_ddot, double* W_e);
+EXPORTED_FUNCTION void customG(double* G, double* q, double* q_dot, double* q_ddot, double* W_e);
+EXPORTED_FUNCTION void customL(double* L, double* q, double* q_dot, double* q_ddot, double* W_e);
 
 #endif
