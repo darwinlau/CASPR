062ee932532c6097a6aae319393b456334a34175
Darwin Lau <darwinlau@hotmail.com>
2020-02-11 11:51:59 +0800

    Added PointsSet class (left out from previous commit)

---
+++ src/Util/Mathematics/Geometry/Shapes/PointsSet.m
@@ -0,0 +1,22 @@
+% Object representation of a set of points
+%
+% Author        : Darwin LAU
+% Created       : 2020
+% Description    :
+classdef PointsSet < handle
+        
+    properties (SetAccess = protected)
+        points = []                 % All points passed in to create the convex polytope
+        numDofs = 0                 % Number of DoFs of the convex polytope
+    end
+    
+    methods        
+        % Points set P are passed in as a matrix where each column
+        % represent one point
+        function ps = PointsSet(P)
+            ps.points = P;
+            % Dimension of point is the number of rows
+            ps.numDofs = size(P, 1);
+        end
+    end
+end
