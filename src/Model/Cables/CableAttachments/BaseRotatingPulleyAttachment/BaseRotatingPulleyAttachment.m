4828b75b67f7e0654e4f440225a46de0753e17d8
Darwin Lau <darwinlau@hotmail.com>
2020-04-20 22:32:00 +0800

    Updated method of computation of base attachment point to be more analytical and avoid the use of PlaneOperations.PlaneIntersection in order to allow for compile mode to be compatible

--- src/Model/Cables/CableAttachments/BaseRotatingPulleyAttachment/BaseRotatingPulleyAttachment.m
+++ src/Model/Cables/CableAttachments/BaseRotatingPulleyAttachment/BaseRotatingPulleyAttachment.m
@@ -60,29 +60,48 @@ classdef BaseRotatingPulleyAttachment < CableAttachmentBase
         %   - r_D: The position of the cable leaving the pulley
         %   - l_arc: length of the arc on the pulley
         %   - r_C: The position of the centre of the pulley location
-        function [r_D, l_arc, r_C] = ComputePulleyLeavingLocation(r_A, v_A, r_B, R)
-            n_plane = cross(v_A, r_B - r_A);
-            p_plane = [0;0;0];
-            n_c_perpendicular = v_A;
-            p_c_perpendicular = [0;0;0];
+        function [r_D, l_arc, r_C] = ComputePulleyLeavingLocation(r_A, v_A, r_B, R)            
+%             n_plane = cross(v_A, r_B - r_A);
+%             p_plane = [0;0;0];
+%             n_c_perpendicular = v_A;
+%             p_c_perpendicular = [0;0;0];
+%             
+%             [p_C, v_C, ~] = PlaneOperations.PlaneIntersection(n_c_perpendicular, p_c_perpendicular, n_plane, p_plane);
+%             a = v_C' * v_C;
+%             b = 2 * p_C' * v_C;
+%             c = p_C' * p_C - R^2;
+%             t = (-b - sqrt(b^2 - 4 * a * c))/(2*a);
+%             r_C = r_A + (p_C + t*v_C);
+%             
+%             n_d_perpendicular = (r_C - r_B);
+%             p_d_perpendicular = [0; 0; -R^2/n_d_perpendicular(3)];
+%             
+%             [p_D, v_D, ~] = PlaneOperations.PlaneIntersection(n_d_perpendicular, p_d_perpendicular, n_plane, p_plane);
+%             a = v_D' * v_D;
+%             b = 2 * p_D' * v_D;
+%             c = p_D' * p_D - R^2;
+%             t = (-b - sqrt(b^2 - 4 * a * c))/(2*a);
+%             r_D = r_C + (p_D + t*v_D)
+%             
+%             n1 = (r_B - r_D)/norm(r_B-r_D);
+%             n2 = v_A/norm(v_A);
+%             l_arc = R * acos(n1' * n2);
             
-            [p_C, v_C, ~] = PlaneOperations.PlaneIntersection(n_c_perpendicular, p_c_perpendicular, n_plane, p_plane);
-            a = v_C' * v_C;
-            b = 2 * p_C' * v_C;
-            c = p_C' * p_C - R^2;
-            t = (-b - sqrt(b^2 - 4 * a * c))/(2*a);
-            r_C = r_A + (p_C + t*v_C);
+            % First calculate the position of the centre of pulley
+            % This must be in the plane of the pulley and also
+            % perpendicular to the rotating axis
+            v_AB = r_B - r_A;
+            n_plane = cross(v_A, v_AB);
+            n_plane_unit = n_plane/norm(n_plane);
+            v_AC = cross(n_plane, v_A);
+            r_C = r_A + R*v_AC/norm(v_AC);
             
-            n_d_perpendicular = (r_C - r_B);
-            p_d_perpendicular = [0; 0; -R^2/n_d_perpendicular(3)];
+            % Next calculate the position of the point leaving pulley
+            rho = R/norm(r_B - r_C);
+            %t1 = r_C + rho^2 * (r_B - r_C) + rho * sqrt(1 - rho^2) * cross(n_plane_unit, r_B - r_C)
+            r_D = r_C + rho^2 * (r_B - r_C) - rho * sqrt(1 - rho^2) * cross(n_plane_unit, r_B - r_C);
             
-            [p_D, v_D, ~] = PlaneOperations.PlaneIntersection(n_d_perpendicular, p_d_perpendicular, n_plane, p_plane);
-            a = v_D' * v_D;
-            b = 2 * p_D' * v_D;
-            c = p_D' * p_D - R^2;
-            t = (-b - sqrt(b^2 - 4 * a * c))/(2*a);
-            r_D = r_C + (p_D + t*v_D);
-            
+            % Finally calculate the arc length (cable length)
             n1 = (r_B - r_D)/norm(r_B-r_D);
             n2 = v_A/norm(v_A);
             l_arc = R * acos(n1' * n2);
