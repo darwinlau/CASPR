a08511815c42c3cec48db12ae9facc11a9a07530
hhcheng13 <chenghunghon@gmail.com>
2020-07-29 18:54:09 +0800

    Fixed bug and speed up the codes

--- GUI/template_scripts/workspace/script_workspace_condition_interference_free_ray_quadsurf.m
+++ GUI/template_scripts/workspace/script_workspace_condition_interference_free_ray_quadsurf.m
@@ -12,20 +12,24 @@ cable_set_id    =   'basic_8_cables';
 modelObj        =   model_config.getModel(cable_set_id);
 
 q_begin         =   modelObj.bodyModel.q_min; 
-q_begin(4:6) = 0;
-% q_begin(1:3) = 0.5;
+% q_begin(4:6) = 0;
+q_begin(1:3) = 0.5;
 q_end = modelObj.bodyModel.q_max; 
-q_end(4:6) = 0;
-% q_end(1:3) = 0.5;
+% q_end(4:6) = 0;
+q_end(1:3) = 0.5;
 %% for rotational first
 % q_begin = [0.3 0 0.5 0 0 0]';
 % q_end = [0.3 1 0.5 0 0 0]';
 % q_begin = [0.35 0.9 0.3 0 0 0]';
 % q_end = [0.35 0.9 0.3 pi/2 0 0]';
-% q_begin = [0.1 0.2 0.55 0 -pi/2 0]';
-% q_end = [0.1 0.2 0.55 0 pi/2 0]';
+% q_begin = [0.1 0.4 0.55 -pi 0 0]';
+% q_end = [0.1 0.4 0.55 pi/2 0 0]';
+% q_begin = [0.1 0.4 0.55 0 -pi/2 0 ]';
+% q_end = [0.1 0.4 0.55 0 0 0]';
+% q_begin = [0.1 0.35 0.55  0 0 -pi/2 ]';
+% q_end = [0.1 0.35 0.55  0 0 pi/2]';
 
-q_step          =   (modelObj.bodyModel.q_max - modelObj.bodyModel.q_min)/3;
+q_step          =   (modelObj.bodyModel.q_max - modelObj.bodyModel.q_min)/20;
 % Set up the workspace simulator
 % First the grid
 uGrid           =   UniformGrid(q_begin,q_end,q_step,'step_size');
@@ -33,35 +37,36 @@ uGrid           =   UniformGrid(q_begin,q_end,q_step,'step_size');
 
 
 %% Sphere
-% QuadSurf.implicit_equation{1} = @(x,y,z) (x-1)^2+(y-0.5)^2+(z-0.6)^2 -0.2*x-0.5*y+0.2*z+0.4*x*y+0.2*y*z-0.3*x*z+0.2
+% QuadSurfImplicitEqu{1} = @(x,y,z) (x-1)^2+(y-0.5)^2+(z-0.6)^2 -0.2*x-0.5*y+0.2*z+0.4*x*y+0.2*y*z-0.3*x*z+0.2
 % QuadSurf.boundary = [-0.1500 1.4706 0 1 0 1];
 
 %% Ball
-% QuadSurf.implicit_equation{1} = @(x,y,z) (x - 0.5)^2 + (y - 0.5)^2 + (z - 0.5)^2 - 0.05
-% QuadSurf.boundary = [0.25 0.75 0.25 0.75 0.25 0.75];
+% QuadSurfImplicitEqu{1} = @(x,y,z) (x - 0.5)^2 + (y - 0.5)^2 + (z - 0.5)^2 - 0.05
+% QuadSurf.boundary{1} = [0.25 0.75 0.25 0.75 0.25 0.75];
 % 
-% QuadSurf.implicit_equation{1} = @(x,y,z) (x - 0.5)^2 + (y - 0.5)^2 + (z - 0.5)^2 - 0.03
-% QuadSurf.boundary = [0.25 0.75 0.25 0.75 0.25 0.75];
+QuadSurfImplicitEqu{1} = @(x,y,z) (x - 0.5)^2 + (y - 0.5)^2 + (z - 0.5)^2 - 0.03
+QuadSurf.boundary{1} = [0.25 0.75 0.25 0.75 0.25 0.75];
 
 
-%% Cylinder           
-% QuadSurf.implicit_equation{1} = @(x,y,z) (x-0.5)^2 + (y-0.75)^2 - 0.02;
+% Cylinder           
+% QuadSurfImplicitEqu{1} = @(x,y,z) (x-0.5)^2 + (y-0.75)^2 - 0.02;
 % QuadSurf.boundary{1} = [0 1 0 1 0 1];
 
-%% Cone
-QuadSurf.implicit_equation{1} = @(x,y,z) (x-0.5)^2/0.005 + (y-0.5)^2 /0.005 - z^2/0.05
-QuadSurf.boundary{1} = [0 1 0 1 0 1];
+% Cone
+% QuadSurfImplicitEqu{2} = @(x,y,z) (x-0.5)^2/0.005 + (y-0.5)^2 /0.005 - z^2/0.05
+% QuadSurf.boundary{2} = [0 1 0 1 0 1];
 
 
 %% Random flat quad surface
-% QuadSurf.implicit_equation{1} = @(x,y,z) (0.01.*x+0.5).^2 + (0.02.*y-0.5).^2 - z.^2 -0.4;
+% QuadSurfImplicitEqu{1} = @(x,y,z) (0.01.*x+0.5).^2 + (0.02.*y-0.5).^2 - z.^2 -0.4;
 % QuadSurf.boundary{1} = [0.45 0.75 0.45 0.75 0 1];
 
 %%
 clf
-ob1 = fimplicit3(QuadSurf.implicit_equation{1},QuadSurf.boundary{1},'FaceColor',[0.5 0.5 0.5],'EdgeColor','none','FaceAlpha',0.4);
+for i = 1:size(QuadSurfImplicitEqu,2)
+obs(i) = fimplicit3(QuadSurfImplicitEqu{i},QuadSurf.boundary{i},'FaceColor',[0.5 0.5 0.5],'EdgeColor','none','FaceAlpha',0.4);
 hold on;
-
+end
 %%  debug use
 % t = linspace(0,1,30);
 % for i = 1:30
@@ -78,17 +83,59 @@ hold on;
 % %     delete(cable_graph(5));
 % %     delete(cable_graph(2));
 % %     delete(cable_graph(7));
+% %     delete(cable_graph(6));
 % %     delete(cable_graph(8));
 % end
-
-
+% 
+% % % % 
 % s = sym('s%d',[1 10]);
-% QuadSurf.implicit_equation{1} = @(x,y,z) s(1)*x^2 + s(2)*y^2 + s(3)*z^2 + ...
+% QuadSurfImplicitEqu{1} = @(x,y,z) s(1)*x^2 + s(2)*y^2 + s(3)*z^2 + ...
 %                                       s(4)*x*y + s(5)*x*z + s(6)*z*y + ...
 %                                       s(7)*x + s(8)*y + s(9)*z + s(10);
-
+% Q = s;
+% QuadSurf.Quad_Matrix{1} = [Q(1) 0.5*Q(4) 0.5*Q(6) 0.5*Q(7);
+%     0.5*Q(4) Q(2) 0.5*Q(5) 0.5*Q(8);
+%     0.5*Q(6) 0.5*Q(5) Q(3) 0.5*Q(9)
+%     0.5*Q(7) 0.5*Q(8) 0.5*Q(9) Q(10)];
 
 %%
+
+%% in the form of xHx^T = 0
+for k = 1:size(QuadSurfImplicitEqu,2)
+syms x y z;
+Q = zeros(1,10);
+[coeff_f,var_f] = coeffs(QuadSurfImplicitEqu{k}(x,y,z));
+for i = 1:size(coeff_f,2)
+    if isequal(var_f(i),x^2)
+        Q(1) = coeff_f(i);
+    elseif isequal(var_f(i),y^2)
+        Q(2) = coeff_f(i);
+    elseif isequal(var_f(i),z^2)
+        Q(3) = coeff_f(i);
+    elseif isequal(var_f(i),x*y)
+        Q(4) = coeff_f(i);
+    elseif isequal(var_f(i),x*z)
+        Q(5) = coeff_f(i);
+    elseif isequal(var_f(i),y*z)
+        Q(6) = coeff_f(i);
+    elseif isequal(var_f(i),x)
+        Q(7) = coeff_f(i);
+    elseif isequal(var_f(i),y)
+        Q(8) = coeff_f(i);
+    elseif isequal(var_f(i),z)
+        Q(9) = coeff_f(i);
+    else
+        Q(10) = coeff_f(i);
+    end
+    
+end
+
+QuadSurf.Quad_Matrix{k} = [Q(1) 0.5*Q(4) 0.5*Q(6) 0.5*Q(7);
+    0.5*Q(4) Q(2) 0.5*Q(5) 0.5*Q(8);
+    0.5*Q(6) 0.5*Q(5) Q(3) 0.5*Q(9)
+    0.5*Q(7) 0.5*Q(8) 0.5*Q(9) Q(10)];
+end
+
 min_segment_percentage = 1;
 w_condition     =   {WorkspaceRayConditionBase.CreateWorkspaceRayCondition(WorkspaceRayConditionType.INTERFERENCE_CABLE_QUADSURF,min_segment_percentage,modelObj,QuadSurf)};
 
